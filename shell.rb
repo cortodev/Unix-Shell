96004aaf637841f7b2996dac28511c5213258a59
ernestrenan <ronan.herry@live.fr>
2012-03-29 00:33:20 +0200

    adding exception handlers

--- shell.rb
+++ shell.rb
@@ -2,22 +2,33 @@
 
 include Process
 
+DESCRIPTOR = {0 => $stdin, 1 => $stdout, 3 => $stderr}
+
 begin
-  print "%>"
-  break if (ligne = gets.downcase.chomp) == 'exit'
-  executeNow, executeAfter = ligne.split('|').map{|cmd| cmd.strip}
+  begin
+    print "%>"
+    ligne = gets.downcase.chomp
+  rescue NoMethodError => e
+    puts "Rentrez une chaine de caractere !"
+    retry
+  end
+  break if ligne == 'exit' 
 
   begin
+    executeAfter ||= ligne
+    executeNow, executeAfter = executeAfter.split('|').map{|cmd| cmd.strip} unless executeAfter.nil?
     commande, redirection = executeNow.split(%r{<|>}).map{|c| c.strip}
+    p "execute #{commande}"
     fork do
       filename = 'tmp'
-      File.open(filename, File::CREAT|File::WRONLY) do |file|
+      File.open(filename, File::CREAT|File::WRONLY|File::TRUNC) do |file|
         res = %x["#{commande}"]
         file.write res
       end
     end
     wait
-    executeNow, executeAfter = executeAfter.split('|').map{|cmd| cmd.strip} unless executeAfter.nil?
   end until executeAfter.nil?
+rescue Exception => e
+  puts e
+  exit
 end while true
-
